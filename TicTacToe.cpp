b115cc4cc6783d642153a65f67f7d2775cfb4831
Haysten Dcosta <haystendc@gmail.com>
2023-10-05 23:46:02 +0530

    Grid display added..

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,56 +1,68 @@
 #include <iostream>
 #include <conio.h>
 #include <ctime>
+#include "textstyling.h"
 
 #define GRID_SIZE 3
 
 bool yourTurn = true;
 int current_row = 0, current_col = 0;
-char grid[GRID_SIZE][GRID_SIZE] = { // grid-board....
-    {' ', ' ', ' '},
-    {' ', ' ', ' '},
-    {' ', ' ', ' '}
-};
+char grid[GRID_SIZE][GRID_SIZE] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
 
-void displayGrid();  // function displays grid....
 void playerMove();   // function for player move....
 void computerMove(); // function for computer move....
 void move(char key); // function to move on the grid (player move)....
 void place(int x, int y, bool turn); // function to place a character in the grid....
-bool checkWinner(char grid[GRID_SIZE][GRID_SIZE]); // function to check for wins....
-bool checkTie(char grid[GRID_SIZE][GRID_SIZE]);    // function to check for draw....
+bool checkTie(char grid[GRID_SIZE][GRID_SIZE]);     // function to check for draw....
+bool checkWinner(char grid[GRID_SIZE][GRID_SIZE]);  // function to check for wins....
+void displayGrid(int current_row, int current_col); // function displays grid....
 
 int main() { // MAIN FUNCTION-----------------------------------------------------------------------------
     while (true) {
-        displayGrid();
+        displayGrid(current_row, current_col);
         playerMove();
-        displayGrid();
         if(checkWinner(grid)) { break; }
         else if(checkTie(grid)) {
             break;
         }
         computerMove();
-        displayGrid();
+        displayGrid(current_row, current_col);
         if(checkWinner(grid)) { break; }
         else if(checkTie(grid)) {
             break;
         }
-        displayGrid();
+        displayGrid(current_row, current_col);
     }
+    std::cout << std::endl; system("pause");
+    displayGrid(-1, -1);
     return 0;
 }
 
-void displayGrid() {
+void displayGrid(int current_row, int current_col) {
     system("cls");
+    // std::cout << "      X-O-X" << std::endl;
+    // std::cout << "+----+-----+----+" << std::endl;  
+    // std::cout << "| " << grid[0][0] << "  |  " << grid[0][1] << "  |  " << grid[0][2] << " |" << std::endl;  
+    // std::cout << "+----+-----+----+" << std::endl;  
+    // std::cout << "| " << grid[1][0] << "  |  " << grid[1][1] << "  |  " << grid[1][2] << " |" << std::endl;
+    // std::cout << "+----+-----+----+" << std::endl;    
+    // std::cout << "| " << grid[2][0] << "  |  " << grid[2][1] << "  |  " << grid[2][2] << " |" << std::endl;  
+    // std::cout << "+----+-----+----+" << std::endl;  
+    // std::cout << std::endl;
     std::cout << std::endl;
-    std::cout << "     |     |     " << std::endl;  
-    std::cout << "  " << grid[0][0] << "  |  " << grid[0][1] << "  |  " << grid[0][2] << "  " << std::endl;  
-    std::cout << "-----|-----|-----" << std::endl;  
-    std::cout << "  " << grid[1][0] << "  |  " << grid[1][1] << "  |  " << grid[1][2] << "  " << std::endl;
-    std::cout << "-----|-----|-----" << std::endl;    
-    std::cout << "  " << grid[2][0] << "  |  " << grid[2][1] << "  |  " << grid[2][2] << "  " << std::endl;  
-    std::cout << "     |     |     " << std::endl;  
-    std::cout << std::endl;
+    std::cout << "+----+----+----+" << std::endl;
+    for(int i=0; i<GRID_SIZE; i++) {
+        for(int j=0; j<GRID_SIZE; j++) {
+            
+            if(i == current_row && j == current_col) {
+                std::cout << "| " << BLACK_TEXT << WHITE_BACKGROUND << grid[i][j] << " " RESET << " ";
+            } else {
+                std::cout << "| " << grid[i][j] << "  ";
+            }
+        }
+        std::cout << "|" << std::endl;
+        std::cout << "+----+----+----+" << std::endl;  
+    }
 }
 void place(int x, int y, bool turn) {
     if(turn) { grid[x][y] = 'X'; } 
@@ -66,7 +78,7 @@ void move(char arrowKey) {
     } else if (arrowKey == 77 && current_col < GRID_SIZE - 1) {
         current_col++;
     }
-    grid[current_row][current_col];
+    displayGrid(current_row, current_col);
 }
 // function here, to check for winner....
 void playerMove() {
